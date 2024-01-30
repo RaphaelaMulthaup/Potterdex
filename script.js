89b90cd46be9a2adbe7f06fb4fe11f3fc0dc0c48
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2024-01-30 12:09:39 +0100

    font and housees

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ async function loadCharacter(){
     let url = 'https://hp-api.onrender.com/api/characters';
     let response = await fetch(url);
     let allCharacters = await response.json();
+    console.log( allCharacters);
     currentCharacter = allCharacters[0];
     console.log(currentCharacter);
     renderCharacterInfo();
@@ -12,6 +13,19 @@ async function loadCharacter(){
 function renderCharacterInfo(){
     document.getElementById('name').innerHTML = currentCharacter['name'];
     document.getElementById('img').setAttribute('href', currentCharacter['image']);
+    if (currentCharacter['house'] != '') {
+            document.getElementById('house').innerHTML = currentCharacter['house'];
+            if (currentCharacter['house'] == 'Gryffindor') {
+                document.getElementById('cardTop').style.backgroundColor = '#A24335';
+            } else if (currentCharacter['house'] == 'Slytherin') {
+                document.getElementById('cardTop').style.backgroundColor = '#1D5B54';
+            } else if (currentCharacter['house'] == 'Ravenclaw') {
+                document.getElementById('cardTop').style.backgroundColor = '#1C477B';
+            } else if (currentCharacter['house'] == 'Hufflepuff') {
+                document.getElementById('cardTop').style.backgroundColor = '#C99C5A';
+            }
+
+    }
 
 }  
 
