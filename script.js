de85e825f2a4856871822aef8bf0456b9aa4e5e4
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2024-02-01 12:33:08 +0100

    finish informations clean renderCode start with css

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
 let currentCharacter;
 
-async function loadCharacter(){
-    // Daten von API abrufen/als JSON/Charakter auswählen
-    let url = 'https://hp-api.onrender.com/api/characters';
-    let response = await fetch(url);
-    let allCharacters = await response.json();
-    console.log( allCharacters);
+async function loadCharacter() {
+  // Daten von API abrufen/als JSON/Charakter auswählen
+  let url = "https://hp-api.onrender.com/api/characters";
+  let response = await fetch(url);
+  let allCharacters = await response.json();
+  console.log(allCharacters);
 
-    for (let index = 0; index < allCharacters.length; index++) {
-        console.log(allCharacters[index]['wand']);
-        
-    }
-    currentCharacter = allCharacters[0];
-    console.log(currentCharacter);
-    renderCharacterInfo();
+  for (let index = 0; index < allCharacters.length; index++) {
+    console.log(allCharacters[index]["wand"]);
+  }
+  currentCharacter = allCharacters[0];
+  console.log(currentCharacter);
+  renderCharacterInfo();
 }
 
-function renderCharacterInfo(){
-    // Name
-    document.getElementById('name').innerHTML = currentCharacter['name'];
-    // Hogwartshaus mit Farbe
-    if (currentCharacter['house'] !== '') {
-            document.getElementById('house').innerHTML = currentCharacter['house'];
-            if (currentCharacter['house'] == 'Gryffindor') {
-                document.getElementById('cardTop').style.backgroundColor = '#A24335';
-            } else if (currentCharacter['house'] == 'Slytherin') {
-                document.getElementById('cardTop').style.backgroundColor = '#1D5B54';
-            } else if (currentCharacter['house'] == 'Ravenclaw') {
-                document.getElementById('cardTop').style.backgroundColor = '#1C477B';
-            } else if (currentCharacter['house'] == 'Hufflepuff') {
-                document.getElementById('cardTop').style.backgroundColor = '#C99C5A';
-            }
+function renderCharacterInfo() {
+  nameAliveHouse();
+  image();
+  wizardWitchMuggle();
+  hogwartsPatronus();
+  wand();
+  generalInformation();
+}
 
+function nameAliveHouse() {
+  // Name
+  document.getElementById("name").innerHTML = currentCharacter["name"];
+  // tot?
+  if (currentCharacter["alive"] == false) {
+    document.getElementById("name").innerHTML += " &dagger;";
+  }
+  // Hogwartshaus mit Farbe
+  if (currentCharacter["house"] !== "") {
+    document.getElementById("house").innerHTML = currentCharacter["house"];
+    if (currentCharacter["house"] == "Gryffindor") {
+      document.getElementById("cardTop").style.backgroundColor = "#A24335";
+    } else if (currentCharacter["house"] == "Slytherin") {
+      document.getElementById("cardTop").style.backgroundColor = "#1D5B54";
+    } else if (currentCharacter["house"] == "Ravenclaw") {
+      document.getElementById("cardTop").style.backgroundColor = "#1C477B";
+    } else if (currentCharacter["house"] == "Hufflepuff") {
+      document.getElementById("cardTop").style.backgroundColor = "#C99C5A";
     }
-    // Bild
-    if (currentCharacter['image'] !== '') {
-            // document.getElementById('img').setAttribute('href', currentCharacter['image']);
-            document.getElementById('cardTop').innerHTML += /*html*/ `
+  }
+}
+
+function image() {
+  // Bild
+  if (currentCharacter["image"] !== "") {
+    // document.getElementById('img').setAttribute('href', currentCharacter['image']);
+    document.getElementById("cardTop").innerHTML += /*html*/ `
             <svg height="200" width="200">
             <defs>
               <pattern
@@ -52,7 +66,7 @@ function renderCharacterInfo(){
                   x="0"
                   y="0"
                   width="200px"
-                  href="${currentCharacter['image']}"
+                  href="${currentCharacter["image"]}"
                 />
               </pattern>
             </defs>
@@ -64,71 +78,133 @@ function renderCharacterInfo(){
             />
           </svg>
             `;
-        }
-    // Zaubere oder Hexe oder Muggel
-    if ((currentCharacter['wizard']==true)&&(currentCharacter['gender']=='male')) {
-        document.getElementById('wizOrMug').innerHTML = 'wizard';
-        ancestry();
-    } else if ((currentCharacter['wizard']==true)&&(currentCharacter['gender']=='female')) {
-        document.getElementById('wizOrMug').innerHTML = 'witch';
-        ancestry();
-    } else if (currentCharacter['ancestry']=='muggle') {
-        document.getElementById('wizOrMug').innerHTML = 'muggle';
-    } else if (currentCharacter['species'] !== 'humen') {
-        document.getElementById('wizOrMug').innerHTML = `${currentCharacter['species']}`;
-    }
+  }
+}
+
+function wizardWitchMuggle() {
+  // Zaubere oder Hexe oder Muggel
+  if (
+    currentCharacter["wizard"] == true &&
+    currentCharacter["gender"] == "male"
+  ) {
+    document.getElementById("wizOrMug").innerHTML = "wizard";
+    ancestry();
+  } else if (
+    currentCharacter["wizard"] == true &&
+    currentCharacter["gender"] == "female"
+  ) {
+    document.getElementById("wizOrMug").innerHTML = "witch";
+    ancestry();
+  } else if (currentCharacter["ancestry"] == "muggle") {
+    document.getElementById("wizOrMug").innerHTML = "muggle";
+  } else if (currentCharacter["species"] !== "humen") {
+    document.getElementById("wizOrMug").innerHTML = `${currentCharacter["species"]}`;
+  }
+}
+
+// Abstammung
+function ancestry() {
+  if (
+    currentCharacter["ancestry"] == "half-blood" ||
+    "muggleborn" ||
+    "pure-blood"
+  ) {
+    document.getElementById(
+      "ancestry"
+    ).innerHTML = `${currentCharacter["ancestry"]}`;
+  }
+}
+
+function hogwartsPatronus(){
     // Hogwarts SchülerIn oder Mitarbeitender
-    if (currentCharacter['hogwartsStudent']) {
-        document.getElementById('hogwartsStudent').innerHTML = 'hogwarts student';
-    }
-    if (currentCharacter['hogwartsStaff']) {
-        document.getElementById('hogwartsStaff').innerHTML = 'hogwarts staff';
-    }
-    // Patronus
-    if (currentCharacter['patronus'] !== '') {
-        document.getElementById('tableWizard').innerHTML += /*html*/ `
+  if (currentCharacter["hogwartsStudent"]) {
+    document.getElementById("hogwartsStudent").innerHTML = "hogwarts student";
+  }
+  if (currentCharacter["hogwartsStaff"]) {
+    document.getElementById("hogwartsStaff").innerHTML = "hogwarts staff";
+  }
+  // Patronus
+  if (currentCharacter["patronus"] !== "") {
+    document.getElementById("tableWizard").innerHTML += /*html*/ `
         <tr>
             <td>patronus</td>
-            <td>${currentCharacter['patronus']}</td>
+            <td>${currentCharacter["patronus"]}</td>
         </tr>
         `;
-    }
-    //Zauberstab
-    if (currentCharacter['wand']['wood'] !== '') {
-        document.getElementById('tableWand').innerHTML += /*html*/ `
+  }
+}
+
+function wand(){
+      //Zauberstab
+  if (currentCharacter["wand"]["wood"] !== "") {
+    document.getElementById("tableWand").innerHTML += /*html*/ `
         <tr>
             <td>wand</td>
         </tr>
         <tr>
             <td>wood</td>
-            <td>${currentCharacter['wand']['wood']}</td>
+            <td>${currentCharacter["wand"]["wood"]}</td>
         </tr>
         `;
-    }
-    if (currentCharacter['wand']['core'] !== '') {
-        document.getElementById('tableWand').innerHTML += /*html*/ `
+  }
+  if (currentCharacter["wand"]["core"] !== "") {
+    document.getElementById("tableWand").innerHTML += /*html*/ `
         <tr>
             <td>core</td>
-            <td>${currentCharacter['wand']['core']}</td>
+            <td>${currentCharacter["wand"]["core"]}</td>
         </tr>
         `;
-    }
-    if (currentCharacter['wand']['length'] !== null) {
-        document.getElementById('tableWand').innerHTML += /*html*/ `
+  }
+  if (currentCharacter["wand"]["length"] !== null) {
+    document.getElementById("tableWand").innerHTML += /*html*/ `
         <tr>
             <td>length</td>
-            <td>${currentCharacter['wand']['length']} inches</td>
+            <td>${currentCharacter["wand"]["length"]} inches</td>
         </tr>
         `;
-    }
+  }
+}
 
-  
-
-
-}  
-// Abstammung
-function ancestry(){
-    if (currentCharacter['ancestry']=='half-blood'||'muggleborn'||'pure-blood') {
-        document.getElementById('ancestry').innerHTML = `${currentCharacter['ancestry']}`;
-    }
+function generalInformation(){
+      // allgemeine Informationen
+  if (currentCharacter["dateOfBirth"] !== null) {
+    document.getElementById("generalInformation").innerHTML += /*html*/ `
+        <tr>
+            <td>date of birth</td>
+            <td>${currentCharacter["dateOfBirth"]}</td>
+        </tr>
+        `;
+  }
+  if (currentCharacter["hairColour"] !== "") {
+    document.getElementById("generalInformation").innerHTML += /*html*/ `
+        <tr>
+            <td>hair colour</td>
+            <td>${currentCharacter["hairColour"]}</td>
+        </tr>
+        `;
+  }
+  if (currentCharacter["eyeColour"] !== "") {
+    document.getElementById("generalInformation").innerHTML += /*html*/ `
+        <tr>
+            <td>eye colour</td>
+            <td>${currentCharacter["eyeColour"]}</td>
+        </tr>
+        `;
+  }
+  if (currentCharacter["gender"] !== "") {
+    document.getElementById("generalInformation").innerHTML += /*html*/ `
+        <tr>
+            <td>gender</td>
+            <td>${currentCharacter["gender"]}</td>
+        </tr>
+        `;
+  }
+  if (currentCharacter["actor"] !== "") {
+    document.getElementById("generalInformation").innerHTML += /*html*/ `
+        <tr>
+            <td>actor</td>
+            <td>${currentCharacter["actor"]}</td>
+        </tr>
+        `;
+  } 
 }
